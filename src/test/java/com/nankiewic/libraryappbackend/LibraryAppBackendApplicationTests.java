31cc57d75571cddea6a62120047c690bfbd2084b
Paweł Nankiewicz <nankiewic@gmail.com>
2021-11-28 11:57:58 +0100

    Initial commit

---
+++ src/test/java/com/nankiewic/libraryappbackend/LibraryAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nankiewic.libraryappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
